e57bb39a63045d37b995a4a5e2db829cda4e11c1
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-11-07 17:00:14 +0100

    fix(ctype/nbr): remove unnecessary casts to unsigned char in cstou64 function

--- src/ctype/nbr/cstou64.c
+++ src/ctype/nbr/cstou64.c
@@ -6,7 +6,7 @@
 /*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 15:43:05 by lfiorell@st       #+#    #+#             */
-/*   Updated: 2025/11/07 15:53:53 by lfiorell@st      ###   ########.fr       */
+/*   Updated: 2025/11/07 17:00:08 by lfiorell@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ bool cstou64(const char* str, uint64_t* out) {
   if (str == NULL || out == NULL) return false;
   uint64_t value = 0;
 
-  while (ft_isspace((unsigned char)*str)) str++;
+  while (ft_isspace(*str)) str++;
   if (*str == '+') {
     str++;
   } else if (*str == '-') {
     return false;
   }
 
-  if (!ft_isdigit((unsigned char)*str)) return false;
+  if (!ft_isdigit(*str)) return false;
 
-  while (ft_isdigit((unsigned char)*str)) {
+  while (ft_isdigit(*str)) {
     uint8_t digit = (uint8_t)(*str - '0');
 
     if (value > UINT64_MAX / 10 ||
